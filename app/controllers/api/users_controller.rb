d524b236cf310ed602fd953294f633dd34174556
MudassarMemon <mudassar95memon@gmail.com>
2023-09-15 09:55:42 -0400

    Create user and session controller and routes for user auth and user create and update functionality

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,39 @@
+class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render 'api/users/index'
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render 'api/users/show'
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user == current_user && @user.update(user_params)
+      render 'api/users/show'
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :gender, :birthday, :password, :bio)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:create, :index, :show, :update]
+    resource :session, only: [:show, :create, :destroy]
+  end
+
+  get '*path', to: "static_pages#frontend_index"
 end
